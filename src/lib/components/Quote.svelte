5879443b2bcf20339ce0aca9a26401222c17cb9f
Brandon Lucas <blu@blu.cx>
2025-07-22 11:15:59 -0400

    feat: display random quote on home and "words" pages

--- src/lib/components/Quote.svelte
+++ src/lib/components/Quote.svelte
@@ -1,18 +1,30 @@
 <script lang="ts">
-	let {
-		text = 'Lorem Ipsum',
-		author = 'Unknown',
-		source = { title: '', date: '', chapter: '', page: '' }
-	} = $props();
+	import quotes from '$lib/data/quotes.json';
+
+	type Quote = {
+		date: string;
+		text: string;
+		source: string;
+	};
+
+	let { quote, index }: { quote?: Quote; index?: number } = $props();
+
+	$effect(() => {
+		if (!quote) {
+			quote = index ? quotes[index] : getRandomQuote();
+		}
+	});
+
+	function getRandomQuote(): Quote {
+		const randomIndex = Math.floor(Math.random() * quotes.length);
+		return quotes[randomIndex];
+	}
 </script>
 
+<span>{quote?.date}</span>
 <blockquote>
-	<span>{text}</span>
-	<footer class="ml-12">
-		- {author}
-		{#if source.title},<i>{source.title}</i>{/if}
-		{#if source.chapter && source.page}
-			<i>{source.chapter}.</i>, p. {source.page}
-		{/if}
-	</footer>
+	<span>{quote?.text}</span>
 </blockquote>
+<ul class="ml-12">
+	<li>{quote?.source}</li>
+</ul>
